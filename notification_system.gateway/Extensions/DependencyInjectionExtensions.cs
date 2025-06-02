722d1f3797c41ee1b1980d95d584b89f3fff8be1
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 06:52:50 +0630

    -

--- notification_system.gateway/Extensions/DependencyInjectionExtensions.cs
+++ notification_system.gateway/Extensions/DependencyInjectionExtensions.cs
@@ -5,61 +5,60 @@ using notification_system.gateway.Services.ServiceDiscovery;
 using Ocelot.DependencyInjection;
 using Ocelot.Provider.Consul;
 
-namespace notification_system.gateway.Extensions
+namespace notification_system.gateway.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddPersistence(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddPersistence(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder
-                .Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile(
-                    $"appsettings.{builder.Environment.EnvironmentName}.json",
-                    optional: false,
-                    reloadOnChange: true
-                )
-                .AddJsonFile(
-                $"ocelot.{builder.Environment.EnvironmentName}.json",
+        builder
+            .Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile(
+                $"appsettings.{builder.Environment.EnvironmentName}.json",
                 optional: false,
                 reloadOnChange: true
-                )
-                .AddEnvironmentVariables();
+            )
+            .AddJsonFile(
+            $"ocelot.{builder.Environment.EnvironmentName}.json",
+            optional: false,
+            reloadOnChange: true
+            )
+            .AddEnvironmentVariables();
 
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-
-            if (!builder.Environment.IsDevelopment())
-            {
-                builder.Services.AddConsul(builder);
-                builder.Services.AddHostedService<ConsulService>();
-            }
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder
-                .Services.AddOcelot()
-                .AddConsul();
-            builder.Services.AddHealthChecks();
-            builder.Services.AddHttpContextAccessor();
-            builder.Services.Configure<AppSetting>(builder.Configuration);
-
-            return services;
-        }
-
-        private static IServiceCollection AddConsul(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            var consulClient = new ConsulClient(config =>
-            {
-                config.Address = new Uri(builder.Configuration["Consul:DiscoveryAddress"]!);
-            });
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-            services.AddSingleton<IConsulClient, ConsulClient>(_ => consulClient);
-            services.AddHostedService<ConsulService>();
-
-            return services;
+        if (!builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddConsul(builder);
+            builder.Services.AddHostedService<ConsulService>();
         }
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder
+            .Services.AddOcelot()
+            .AddConsul();
+        builder.Services.AddHealthChecks();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.Configure<AppSetting>(builder.Configuration);
+
+        return services;
+    }
+
+    private static IServiceCollection AddConsul(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        var consulClient = new ConsulClient(config =>
+        {
+            config.Address = new Uri(builder.Configuration["Consul:DiscoveryAddress"]!);
+        });
+
+        services.AddSingleton<IConsulClient, ConsulClient>(_ => consulClient);
+        services.AddHostedService<ConsulService>();
+
+        return services;
     }
 }
