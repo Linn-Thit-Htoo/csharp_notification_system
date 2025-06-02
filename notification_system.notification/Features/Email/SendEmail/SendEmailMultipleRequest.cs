05ad8f487b1979c0f8e4b5240d84fb055dad6ed3
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:42:16 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendEmailMultipleRequest.cs
+++ notification_system.notification/Features/Email/SendEmail/SendEmailMultipleRequest.cs
@@ -1,14 +1,13 @@
 ﻿using notification_system.notification.Models;
 
-namespace notification_system.notification.Features.Email.SendEmail
+namespace notification_system.notification.Features.Email.SendEmail;
+
+public class SendEmailMultipleRequest
 {
-    public class SendEmailMultipleRequest
-    {
-        public string Subject { get; set; }
-        public string HtmlContent { get; set; }
-        public List<string>? ToEmails { get; set; }
-        public List<string>? CcEmails { get; set; }
-        public List<string>? BccEmails { get; set; }
-        public List<FileModel>? Files { get; set; }
-    }
+    public string Subject { get; set; }
+    public string HtmlContent { get; set; }
+    public List<string>? ToEmails { get; set; }
+    public List<string>? CcEmails { get; set; }
+    public List<string>? BccEmails { get; set; }
+    public List<FileModel>? Files { get; set; }
 }
