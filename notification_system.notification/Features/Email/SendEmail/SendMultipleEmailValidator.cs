ad23560d860eec07b107f5e50c056b1280ef0ba6
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:42:44 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendMultipleEmailValidator.cs
+++ notification_system.notification/Features/Email/SendEmail/SendMultipleEmailValidator.cs
@@ -7,14 +7,17 @@ public class SendMultipleEmailValidator : AbstractValidator<SendEmailMultipleReq
     public SendMultipleEmailValidator()
     {
         RuleFor(x => x.Subject)
-            .NotEmpty().WithMessage("Subject cannot be empty.")
-            .NotNull().WithMessage("Subject cannto be null.");
+            .NotEmpty()
+            .WithMessage("Subject cannot be empty.")
+            .NotNull()
+            .WithMessage("Subject cannto be null.");
 
         RuleFor(x => x.HtmlContent)
-            .NotEmpty().WithMessage("Html Content cannot be empty.")
-            .NotNull().WithMessage("Html Content cannot be null.");
+            .NotEmpty()
+            .WithMessage("Html Content cannot be empty.")
+            .NotNull()
+            .WithMessage("Html Content cannot be null.");
 
-        RuleFor(x => x.ToEmails)
-            .NotNull().WithMessage("To Emails cannot be null.");
+        RuleFor(x => x.ToEmails).NotNull().WithMessage("To Emails cannot be null.");
     }
 }
