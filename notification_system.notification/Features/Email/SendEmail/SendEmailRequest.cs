8db490f80bb0df4dbb57ae8c4006e337a5f001d6
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:42:24 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendEmailRequest.cs
+++ notification_system.notification/Features/Email/SendEmail/SendEmailRequest.cs
@@ -1,14 +1,13 @@
 ﻿using notification_system.notification.Models;
 
-namespace notification_system.notification.Features.Email.SendEmail
+namespace notification_system.notification.Features.Email.SendEmail;
+
+public class SendEmailRequest
 {
-    public class SendEmailRequest
-    {
-        public string Subject { get; set; }
-        public string HtmlContent { get; set; }
-        public string ToEmail { get; set; }
-        public string? CcEmail { get; set; }
-        public string? BccEmail { get; set; }
-        public List<FileModel>? Files { get; set; }
-    }
+    public string Subject { get; set; }
+    public string HtmlContent { get; set; }
+    public string ToEmail { get; set; }
+    public string? CcEmail { get; set; }
+    public string? BccEmail { get; set; }
+    public List<FileModel>? Files { get; set; }
 }
