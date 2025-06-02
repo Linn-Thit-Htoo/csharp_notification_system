3082a8913c0691c9fd0540646b2e19181f65941b
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:41:51 +0630

    -

--- notification_system.notification/Features/Email/Core/EmailController.cs
+++ notification_system.notification/Features/Email/Core/EmailController.cs
@@ -1,31 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using notification_system.notification.Features.Email.SendEmail;
 
-namespace notification_system.notification.Features.Email.Core
+namespace notification_system.notification.Features.Email.Core;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class EmailController : BaseController
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    public class EmailController : BaseController
+    private readonly BL_SendEmail _bL_SendEmail;
+
+    public EmailController(BL_SendEmail bL_SendEmail)
     {
-        private readonly BL_SendEmail _bL_SendEmail;
+        _bL_SendEmail = bL_SendEmail;
+    }
 
-        public EmailController(BL_SendEmail bL_SendEmail)
-        {
-            _bL_SendEmail = bL_SendEmail;
-        }
+    [HttpPost("SendSingleEmail")]
+    public async Task<IActionResult> SendSingleEmail(SendEmailRequest request, CancellationToken cs)
+    {
+        var result = await _bL_SendEmail.SendSingleEmailAsync(request, cs);
+        return Content(result);
+    }
 
-        [HttpPost("SendSingleEmail")]
-        public async Task<IActionResult> SendSingleEmail(SendEmailRequest request, CancellationToken cs)
-        {
-            var result = await _bL_SendEmail.SendSingleEmailAsync(request, cs);
-            return Content(result);
-        }
-
-        [HttpPost(("SendMultipleEmail"))]
-        public async Task<IActionResult> SendMultipleEmail(SendEmailMultipleRequest request, CancellationToken cs)
-        {
-            var result = await _bL_SendEmail.SendMultipleEmailAsync(request, cs);
-            return Content(result);
-        }
+    [HttpPost(("SendMultipleEmail"))]
+    public async Task<IActionResult> SendMultipleEmail(SendEmailMultipleRequest request, CancellationToken cs)
+    {
+        var result = await _bL_SendEmail.SendMultipleEmailAsync(request, cs);
+        return Content(result);
     }
 }
