4d4fe83ca7fecc62bb15c3848c7e07fb94e98580
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:43:16 +0630

    -

--- notification_system.notification/Utils/Enricher.cs
+++ notification_system.notification/Utils/Enricher.cs
@@ -2,34 +2,33 @@
 using Serilog;
 using System.Security.Claims;
 
-namespace notification_system.notification.Utils
+namespace notification_system.notification.Utils;
+
+public class Enricher
 {
-    public class Enricher
+    internal static void HttpRequestEnricher(
+        IDiagnosticContext diagnosticContext,
+        HttpContext httpContext
+    )
     {
-        internal static void HttpRequestEnricher(
-            IDiagnosticContext diagnosticContext,
-            HttpContext httpContext
-        )
+        var httpContextInfo = new HttpContextInfo
         {
-            var httpContextInfo = new HttpContextInfo
-            {
-                Protocol = httpContext.Request.Protocol,
-                Scheme = httpContext.Request.Scheme,
-                IpAddress = httpContext.Connection.RemoteIpAddress!.ToString(),
-                Host = httpContext.Request.Host.ToString(),
-                User = GetUserInfo(httpContext.User),
-            };
+            Protocol = httpContext.Request.Protocol,
+            Scheme = httpContext.Request.Scheme,
+            IpAddress = httpContext.Connection.RemoteIpAddress!.ToString(),
+            Host = httpContext.Request.Host.ToString(),
+            User = GetUserInfo(httpContext.User),
+        };
 
-            diagnosticContext.Set("HttpContext", httpContextInfo, true);
-        }
+        diagnosticContext.Set("HttpContext", httpContextInfo, true);
+    }
 
-        private static string GetUserInfo(ClaimsPrincipal user)
+    private static string GetUserInfo(ClaimsPrincipal user)
+    {
+        if (user.Identity != null && user.Identity.IsAuthenticated)
         {
-            if (user.Identity != null && user.Identity.IsAuthenticated)
-            {
-                return user.Identity.Name!;
-            }
-            return Environment.UserName;
+            return user.Identity.Name!;
         }
+        return Environment.UserName;
     }
 }
