3e0a8a9c4f30095b97df5e1a0c068bc7d254044b
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:42:52 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendSingleEmailValidator.cs
+++ notification_system.notification/Features/Email/SendEmail/SendSingleEmailValidator.cs
@@ -7,16 +7,23 @@ public class SendSingleEmailValidator : AbstractValidator<SendEmailRequest>
     public SendSingleEmailValidator()
     {
         RuleFor(x => x.Subject)
-            .NotEmpty().WithMessage("Subject cannot be empty.")
-            .NotNull().WithMessage("Subject cannto be null.");
+            .NotEmpty()
+            .WithMessage("Subject cannot be empty.")
+            .NotNull()
+            .WithMessage("Subject cannto be null.");
 
         RuleFor(x => x.HtmlContent)
-            .NotEmpty().WithMessage("Html Content cannot be empty.")
-            .NotNull().WithMessage("Html Content cannot be null.");
+            .NotEmpty()
+            .WithMessage("Html Content cannot be empty.")
+            .NotNull()
+            .WithMessage("Html Content cannot be null.");
 
         RuleFor(x => x.ToEmail)
-            .NotEmpty().WithMessage("To Email cannot be empty.")
-            .NotNull().WithMessage("To Email cannot be null.")
-            .EmailAddress().WithMessage("Email is invailid.");
+            .NotEmpty()
+            .WithMessage("To Email cannot be empty.")
+            .NotNull()
+            .WithMessage("To Email cannot be null.")
+            .EmailAddress()
+            .WithMessage("Email is invailid.");
     }
 }
