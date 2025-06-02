c67367e8cb923e729b0dbc33dfca44324047671a
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:43:06 +0630

    -

--- notification_system.notification/Utils/BaseResponse.cs
+++ notification_system.notification/Utils/BaseResponse.cs
@@ -1,71 +1,70 @@
 ﻿using System.Net;
 
-namespace notification_system.notification.Utils
+namespace notification_system.notification.Utils;
+
+public class BaseResponse<T>
 {
-    public class BaseResponse<T>
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; } = null!;
+    public T? Data { get; set; }
+
+    public static BaseResponse<T> Success(string message = "Success")
     {
-        public HttpStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; } = null!;
-        public T? Data { get; set; }
-
-        public static BaseResponse<T> Success(string message = "Success")
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                Message = message,
-                IsSuccess = true,
-                StatusCode = HttpStatusCode.OK,
-            };
-        }
+            Message = message,
+            IsSuccess = true,
+            StatusCode = HttpStatusCode.OK,
+        };
+    }
 
-        public static BaseResponse<T> Success(T data, string message = "Success")
+    public static BaseResponse<T> Success(T data, string message = "Success")
+    {
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                Data = data,
-                IsSuccess = true,
-                Message = message,
-                StatusCode = HttpStatusCode.OK,
-            };
-        }
+            Data = data,
+            IsSuccess = true,
+            Message = message,
+            StatusCode = HttpStatusCode.OK,
+        };
+    }
 
-        public static BaseResponse<T> Fail(
-            string message = "Fail.",
-            HttpStatusCode statusCode = HttpStatusCode.BadRequest
-        )
+    public static BaseResponse<T> Fail(
+        string message = "Fail.",
+        HttpStatusCode statusCode = HttpStatusCode.BadRequest
+    )
+    {
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                IsSuccess = false,
-                Message = message,
-                StatusCode = statusCode,
-            };
-        }
+            IsSuccess = false,
+            Message = message,
+            StatusCode = statusCode,
+        };
+    }
 
-        public static BaseResponse<T> Fail(Exception ex)
+    public static BaseResponse<T> Fail(Exception ex)
+    {
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                IsSuccess = false,
-                Message = ex.ToString(),
-                StatusCode = HttpStatusCode.InternalServerError,
-            };
-        }
+            IsSuccess = false,
+            Message = ex.ToString(),
+            StatusCode = HttpStatusCode.InternalServerError,
+        };
+    }
 
-        public static BaseResponse<T> NotFound(string message = "No data found.")
-        {
-            return BaseResponse<T>.Fail(message, HttpStatusCode.NotFound);
-        }
+    public static BaseResponse<T> NotFound(string message = "No data found.")
+    {
+        return BaseResponse<T>.Fail(message, HttpStatusCode.NotFound);
+    }
 
-        public static BaseResponse<T> Duplicate(string message = "Duplicate data.")
+    public static BaseResponse<T> Duplicate(string message = "Duplicate data.")
+    {
+        return new BaseResponse<T>
         {
-            return new BaseResponse<T>
-            {
-                IsSuccess = false,
-                Message = message,
-                StatusCode = HttpStatusCode.Conflict,
-            };
-        }
+            IsSuccess = false,
+            Message = message,
+            StatusCode = HttpStatusCode.Conflict,
+        };
     }
 }
