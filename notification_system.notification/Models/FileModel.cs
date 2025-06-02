ac37e2debeb8d6e3ac0db6cf4324c8ecfd5eae4f
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:43:29 +0630

    -

--- notification_system.notification/Models/FileModel.cs
+++ notification_system.notification/Models/FileModel.cs
@@ -1,9 +1,8 @@
-﻿namespace notification_system.notification.Models
+﻿namespace notification_system.notification.Models;
+
+public class FileModel
 {
-    public class FileModel
-    {
-        public string FileName { get; set; }
-        public byte[] Content { get; set; }
-        public string ContentType { get; set; }
-    }
+    public string FileName { get; set; }
+    public byte[] Content { get; set; }
+    public string ContentType { get; set; }
 }
