189b99f37eac0e74c2cae5b7a20db11310bafde1
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:42:50 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendSingleEmailValidator.cs
+++ notification_system.notification/Features/Email/SendEmail/SendSingleEmailValidator.cs
@@ -1,23 +1,22 @@
 ﻿using FluentValidation;
 
-namespace notification_system.notification.Features.Email.SendEmail
+namespace notification_system.notification.Features.Email.SendEmail;
+
+public class SendSingleEmailValidator : AbstractValidator<SendEmailRequest>
 {
-    public class SendSingleEmailValidator : AbstractValidator<SendEmailRequest>
+    public SendSingleEmailValidator()
     {
-        public SendSingleEmailValidator()
-        {
-            RuleFor(x => x.Subject)
-                .NotEmpty().WithMessage("Subject cannot be empty.")
-                .NotNull().WithMessage("Subject cannto be null.");
+        RuleFor(x => x.Subject)
+            .NotEmpty().WithMessage("Subject cannot be empty.")
+            .NotNull().WithMessage("Subject cannto be null.");
 
-            RuleFor(x => x.HtmlContent)
-                .NotEmpty().WithMessage("Html Content cannot be empty.")
-                .NotNull().WithMessage("Html Content cannot be null.");
+        RuleFor(x => x.HtmlContent)
+            .NotEmpty().WithMessage("Html Content cannot be empty.")
+            .NotNull().WithMessage("Html Content cannot be null.");
 
-            RuleFor(x => x.ToEmail)
-                .NotEmpty().WithMessage("To Email cannot be empty.")
-                .NotNull().WithMessage("To Email cannot be null.")
-                .EmailAddress().WithMessage("Email is invailid.");
-        }
+        RuleFor(x => x.ToEmail)
+            .NotEmpty().WithMessage("To Email cannot be empty.")
+            .NotNull().WithMessage("To Email cannot be null.")
+            .EmailAddress().WithMessage("Email is invailid.");
     }
 }
