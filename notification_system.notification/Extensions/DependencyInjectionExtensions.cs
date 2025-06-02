c960858cf631390c5e7a1c9f4b2e417852094903
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:41:16 +0630

    -

--- notification_system.notification/Extensions/DependencyInjectionExtensions.cs
+++ notification_system.notification/Extensions/DependencyInjectionExtensions.cs
@@ -22,68 +22,85 @@ using notification_system.notification.Services.SMSServices;
 using notification_system.notification.Utils;
 using Serilog;
 
-namespace notification_system.notification.Extensions
+namespace notification_system.notification.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddPersistence(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddPersistence(this IServiceCollection services, WebApplicationBuilder builder)
+        builder
+            .Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile(
+                $"appsettings.{builder.Environment.EnvironmentName}.json",
+                optional: false,
+                reloadOnChange: true
+            )
+            .AddEnvironmentVariables();
+
+        builder
+            .Services.AddControllers()
+            .ConfigureApiBehaviorOptions(opt =>
+            {
+                opt.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context
+                        .ModelState.Where(ms => ms.Value!.Errors.Any())
+                        .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
+                        .ToList();
+
+                    var errorMessage = string.Join(" ", errors);
+                    var result = BaseResponse<object>.Fail(errorMessage);
+
+                    return new OkObjectResult(result);
+                };
+            })
+            .AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+                opt.JsonSerializerOptions.DictionaryKeyPolicy = null;
+            });
+
+        builder.Services.AddDbContext<NotificationDbContext>((sp, opt) =>
         {
-            builder
-                .Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile(
-                    $"appsettings.{builder.Environment.EnvironmentName}.json",
-                    optional: false,
-                    reloadOnChange: true
-                )
-                .AddEnvironmentVariables();
+            var setting = sp.GetRequiredService<IOptions<AppSetting>>().Value;
+            opt.UseSqlServer(setting.ConnectionStrings.NotiConnection);
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            builder
-                .Services.AddControllers()
-                .ConfigureApiBehaviorOptions(opt =>
-                {
-                    opt.InvalidModelStateResponseFactory = context =>
-                    {
-                        var errors = context
-                            .ModelState.Where(ms => ms.Value!.Errors.Any())
-                            .SelectMany(ms => ms.Value!.Errors.Select(e => e.ErrorMessage))
-                            .ToList();
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(
+                "CORSPolicy",
+                builder =>
+                    builder
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .SetIsOriginAllowed((hosts) => true)
+            );
+        });
 
-                        var errorMessage = string.Join(" ", errors);
-                        var result = BaseResponse<object>.Fail(errorMessage);
+        FirebaseApp.Create(new AppOptions()
+        {
+            Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "notification-system-b95f7-firebase-adminsdk-fbsvc-e613046e55.json")),
+        });
 
-                        return new OkObjectResult(result);
-                    };
-                })
-                .AddJsonOptions(opt =>
-                {
-                    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-                    opt.JsonSerializerOptions.DictionaryKeyPolicy = null;
-                });
-
-            builder.Services.AddDbContext<NotificationDbContext>((sp, opt) =>
+        builder.Host.UseSerilog(
+            (context, config) =>
             {
-                var setting = sp.GetRequiredService<IOptions<AppSetting>>().Value;
-                opt.UseSqlServer(setting.ConnectionStrings.NotiConnection);
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
+                config
+                    .ReadFrom.Configuration(context.Configuration)
+                    .WriteTo.Console()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+                    .Enrich.WithEnvironmentName();
+            }
+        );
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(
-                    "CORSPolicy",
-                    builder =>
-                        builder
-                            .AllowAnyMethod()
-                            .AllowAnyHeader()
-                            .AllowCredentials()
-                            .SetIsOriginAllowed((hosts) => true)
-                );
-            });
-
-            FirebaseApp.Create(new AppOptions()
-            {
-                Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "notification-system-b95f7-firebase-adminsdk-fbsvc-e613046e55.json")),
-            });
+        if (!builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddConsul(builder);
+            builder.Services.AddHostedService<ConsulService>();
 
             builder.Host.UseSerilog(
                 (context, config) =>
@@ -93,76 +110,58 @@ namespace notification_system.notification.Extensions
                         .WriteTo.Console()
                         .Enrich.FromLogContext()
                         .Enrich.WithMachineName()
-                        .Enrich.WithEnvironmentName();
+                        .Enrich.WithEnvironmentName()
+                        .WriteTo.Seq("http://seq:5341");
                 }
             );
-
-            if (!builder.Environment.IsDevelopment())
-            {
-                builder.Services.AddConsul(builder);
-                builder.Services.AddHostedService<ConsulService>();
-
-                builder.Host.UseSerilog(
-                    (context, config) =>
-                    {
-                        config
-                            .ReadFrom.Configuration(context.Configuration)
-                            .WriteTo.Console()
-                            .Enrich.FromLogContext()
-                            .Enrich.WithMachineName()
-                            .Enrich.WithEnvironmentName()
-                            .WriteTo.Seq("http://seq:5341");
-                    }
-                );
-            }
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddHealthChecks();
-            builder.Services.AddHttpContextAccessor();
-            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-            builder.Services.AddProblemDetails();
-            builder.Services.Configure<AppSetting>(builder.Configuration);
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddScoped<IEmailService, EmailService>();
-            builder.Services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
-            builder.Services.AddBusinessLogicServices();
-            builder.Services.AddDataAccessServices();
-            builder.Services.AddHostedService<RabbitMQService>();
-            builder.Services.AddScoped<IPushNotiService, PushNotiService>();
-            builder.Services.AddScoped<ITwilioService, TwilioService>();
-
-            return services;
         }
 
-        private static IServiceCollection AddConsul(this IServiceCollection services, WebApplicationBuilder builder)
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHealthChecks();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        builder.Services.AddProblemDetails();
+        builder.Services.Configure<AppSetting>(builder.Configuration);
+        builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        builder.Services.AddScoped<IEmailService, EmailService>();
+        builder.Services.AddValidatorsFromAssembly(typeof(DependencyInjectionExtensions).Assembly);
+        builder.Services.AddBusinessLogicServices();
+        builder.Services.AddDataAccessServices();
+        builder.Services.AddHostedService<RabbitMQService>();
+        builder.Services.AddScoped<IPushNotiService, PushNotiService>();
+        builder.Services.AddScoped<ITwilioService, TwilioService>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddConsul(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        var consulClient = new ConsulClient(config =>
         {
-            var consulClient = new ConsulClient(config =>
-            {
-                config.Address = new Uri(builder.Configuration["Consul:DiscoveryAddress"]!);
-            });
+            config.Address = new Uri(builder.Configuration["Consul:DiscoveryAddress"]!);
+        });
 
-            services.AddSingleton<IConsulClient, ConsulClient>(_ => consulClient);
-            services.AddHostedService<ConsulService>();
+        services.AddSingleton<IConsulClient, ConsulClient>(_ => consulClient);
+        services.AddHostedService<ConsulService>();
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
-        {
-            services.AddScoped<BL_SendEmail>();
-            services.AddScoped<BL_RequestOtp>();
-            services.AddScoped<BL_VerifyOtp>();
-            services.AddScoped<BL_PushNoti>();
-            services.AddScoped<BL_SendSMS>();
-            return services;
-        }
+    private static IServiceCollection AddBusinessLogicServices(this IServiceCollection services)
+    {
+        services.AddScoped<BL_SendEmail>();
+        services.AddScoped<BL_RequestOtp>();
+        services.AddScoped<BL_VerifyOtp>();
+        services.AddScoped<BL_PushNoti>();
+        services.AddScoped<BL_SendSMS>();
+        return services;
+    }
 
-        private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            services.AddScoped<DA_RequestOtp>();
-            services.AddScoped<DA_VerifyOtp>();
-            return services;
-        }
+    private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        services.AddScoped<DA_RequestOtp>();
+        services.AddScoped<DA_VerifyOtp>();
+        return services;
     }
 }
