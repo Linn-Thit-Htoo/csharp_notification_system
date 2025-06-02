9c4eb54909296231b45227a8aa76ad76555c7451
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:43:46 +0630

    -

--- notification_system.notification/Features/BaseController.cs
+++ notification_system.notification/Features/BaseController.cs
@@ -2,15 +2,14 @@
 using Microsoft.AspNetCore.Mvc;
 using notification_system.notification.Extensions;
 
-namespace notification_system.notification.Features
+namespace notification_system.notification.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.ToJson(), "application/json");
-        }
+        return Content(obj.ToJson(), "application/json");
     }
 }
