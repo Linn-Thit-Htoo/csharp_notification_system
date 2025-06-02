ec744a657bdf78e66e2cf5ddde4479243190657d
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:42:41 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendMultipleEmailValidator.cs
+++ notification_system.notification/Features/Email/SendEmail/SendMultipleEmailValidator.cs
@@ -1,21 +1,20 @@
 ﻿using FluentValidation;
 
-namespace notification_system.notification.Features.Email.SendEmail
+namespace notification_system.notification.Features.Email.SendEmail;
+
+public class SendMultipleEmailValidator : AbstractValidator<SendEmailMultipleRequest>
 {
-    public class SendMultipleEmailValidator : AbstractValidator<SendEmailMultipleRequest>
+    public SendMultipleEmailValidator()
     {
-        public SendMultipleEmailValidator()
-        {
-            RuleFor(x => x.Subject)
-                .NotEmpty().WithMessage("Subject cannot be empty.")
-                .NotNull().WithMessage("Subject cannto be null.");
+        RuleFor(x => x.Subject)
+            .NotEmpty().WithMessage("Subject cannot be empty.")
+            .NotNull().WithMessage("Subject cannto be null.");
 
-            RuleFor(x => x.HtmlContent)
-                .NotEmpty().WithMessage("Html Content cannot be empty.")
-                .NotNull().WithMessage("Html Content cannot be null.");
+        RuleFor(x => x.HtmlContent)
+            .NotEmpty().WithMessage("Html Content cannot be empty.")
+            .NotNull().WithMessage("Html Content cannot be null.");
 
-            RuleFor(x => x.ToEmails)
-                .NotNull().WithMessage("To Emails cannot be null.");
-        }
+        RuleFor(x => x.ToEmails)
+            .NotNull().WithMessage("To Emails cannot be null.");
     }
 }
