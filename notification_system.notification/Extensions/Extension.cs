06ca4ab288e7d94e62cd18212a1630355c5de2bd
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-02 22:41:32 +0630

    -

--- notification_system.notification/Extensions/Extension.cs
+++ notification_system.notification/Extensions/Extension.cs
@@ -4,7 +4,11 @@ namespace notification_system.notification.Extensions;
 
 public static class Extension
 {
-    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+    public static string ToJson(this object obj) =>
+        JsonConvert.SerializeObject(obj, Formatting.Indented);
+
     public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
-    public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+
+    public static bool IsNullOrEmpty(this string str) =>
+        string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
 }
