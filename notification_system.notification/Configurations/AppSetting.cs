4cc2c9f9de17a216ab2ee6011ad9fe3c4aa684c8
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-03 12:32:25 +0630

    feat: in progress kafka consumer

--- notification_system.notification/Configurations/AppSetting.cs
+++ notification_system.notification/Configurations/AppSetting.cs
@@ -7,6 +7,7 @@ public class AppSetting
     public Sendgrid SendGrid { get; set; }
     public Otpconfig OtpConfig { get; set; }
     public Rabbitmq RabbitMQ { get; set; }
+    public Kafka Kafka { get; set; }
     public Consul Consul { get; set; }
     public Twilio Twilio { get; set; }
 }
@@ -52,6 +53,49 @@ public class Queuelist
     public string Exchange { get; set; }
     public string Queue { get; set; }
     public string RoutingKey { get; set; }
+}
+
+public class Kafka
+{
+    public string BootstrapServers { get; set; }
+    public Email Email { get; set; }
+    public SMS SMS { get; set; }
+}
+
+public class Email
+{
+    public Singleemail SingleEmail { get; set; }
+    public Multipleemail MultipleEmail { get; set; }
+}
+
+public class Singleemail
+{
+    public string Topic { get; set; }
+    public string GroupId { get; set; }
+}
+
+public class Multipleemail
+{
+    public string Topic { get; set; }
+    public string GroupId { get; set; }
+}
+
+public class SMS
+{
+    public Singlesms SingleSMS { get; set; }
+    public Multiplesms MultipleSMS { get; set; }
+}
+
+public class Singlesms
+{
+    public string Topic { get; set; }
+    public string GroupId { get; set; }
+}
+
+public class Multiplesms
+{
+    public string Topic { get; set; }
+    public string GroupId { get; set; }
 }
 
 public class Consul
---
+++ notification_system.notification/Services/Kafka/SingleEmailConsumerService.cs
@@ -0,0 +1,6 @@
+﻿namespace notification_system.notification.Services.Kafka
+{
+    public class SingleEmailConsumerService
+    {
+    }
+}
