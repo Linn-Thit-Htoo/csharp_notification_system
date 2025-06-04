99e57c241f9ea73c31fcdc9ac455134f9cb3984e
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-04 08:29:44 +0630

    -

--- notification_system.notification/Services/Kafka/MultipleEmailConsumerService.cs
+++ notification_system.notification/Services/Kafka/MultipleEmailConsumerService.cs
@@ -6,53 +6,52 @@ using notification_system.notification.Features.Email.SendEmail;
 using notification_system.notification.Services.EmailServices;
 using static notification_system.notification.Extensions.Extension;
 
-namespace notification_system.notification.Services.Kafka
+namespace notification_system.notification.Services.Kafka;
+
+public class MultipleEmailConsumerService : BackgroundService
 {
-    public class MultipleEmailConsumerService : BackgroundService
+    private readonly IConsumer<Ignore, string> _consumer;
+    private readonly ILogger<MultipleEmailConsumerService> _logger;
+    private readonly AppSetting _appSetting;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public MultipleEmailConsumerService(ILogger<MultipleEmailConsumerService> logger, IOptions<AppSetting> setting, IServiceScopeFactory serviceScopeFactory)
     {
-        private readonly IConsumer<Ignore, string> _consumer;
-        private readonly ILogger<MultipleEmailConsumerService> _logger;
-        private readonly AppSetting _appSetting;
-        private readonly IServiceScopeFactory _serviceScopeFactory;
+        _logger = logger;
+        _appSetting = setting.Value;
 
-        public MultipleEmailConsumerService(ILogger<MultipleEmailConsumerService> logger, IOptions<AppSetting> setting, IServiceScopeFactory serviceScopeFactory)
+        var consumerConfig = new ConsumerConfig
         {
-            _logger = logger;
-            _appSetting = setting.Value;
+            BootstrapServers = _appSetting.Kafka.BootstrapServers,
+            GroupId = _appSetting.Kafka.Email.MultipleEmail.GroupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
 
-            var consumerConfig = new ConsumerConfig
-            {
-                BootstrapServers = _appSetting.Kafka.BootstrapServers,
-                GroupId = _appSetting.Kafka.Email.MultipleEmail.GroupId,
-                AutoOffsetReset = AutoOffsetReset.Earliest
-            };
+        _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+        _serviceScopeFactory = serviceScopeFactory;
+    }
 
-            _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
-            _serviceScopeFactory = serviceScopeFactory;
-        }
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        //await Extension.EnsureTopicExistsAsync(_appSetting.Kafka.BootstrapServers, _appSetting.Kafka.Email.SingleEmail.Topic);
+        _consumer.Subscribe(_appSetting.Kafka.Email.MultipleEmail.Topic);
 
-        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            //await Extension.EnsureTopicExistsAsync(_appSetting.Kafka.BootstrapServers, _appSetting.Kafka.Email.SingleEmail.Topic);
-            _consumer.Subscribe(_appSetting.Kafka.Email.MultipleEmail.Topic);
-
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                try
-                {
-                    var scope = _serviceScopeFactory.CreateScope();
-                    var emailService = scope.ServiceProvider.GetRequiredService<IEmailService>();
-                    var consumeResult = _consumer.Consume(stoppingToken);
-                    var message = consumeResult.Message.Value;
-                    var request = message.ToObject<SendMultipleEmailRequest>();
+                var scope = _serviceScopeFactory.CreateScope();
+                var emailService = scope.ServiceProvider.GetRequiredService<IEmailService>();
+                var consumeResult = _consumer.Consume(stoppingToken);
+                var message = consumeResult.Message.Value;
+                var request = message.ToObject<SendMultipleEmailRequest>();
 
-                    await emailService.SendMultipleEmailAysnc(request, stoppingToken);
-                    _logger.LogInformation($"Received Multiple Email Consumer: {message}");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError($"Error processing Multiple Email Consumer: {ex.Message}");
-                }
+                await emailService.SendMultipleEmailAysnc(request, stoppingToken);
+                _logger.LogInformation($"Received Multiple Email Consumer: {message}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error processing Multiple Email Consumer: {ex.Message}");
             }
         }
     }
