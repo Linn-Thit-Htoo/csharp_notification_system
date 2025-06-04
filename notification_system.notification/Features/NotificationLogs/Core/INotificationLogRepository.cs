0b6fa5ef2108d0bb782db0394fa57ffa93cb651a
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-04 19:53:03 +0630

    -

--- notification_system.notification/Features/NotificationLogs/Core/INotificationLogRepository.cs
+++ notification_system.notification/Features/NotificationLogs/Core/INotificationLogRepository.cs
@@ -1,7 +1,6 @@
 ﻿using notification_system.notification.Entities;
 using notification_system.notification.Persistence.Base;
 
-namespace notification_system.notification.Features.NotificationLogs.Core
-{
-    public interface INotificationLogRepository : IRepositoryBase<TblNotificationLog> { }
-}
+namespace notification_system.notification.Features.NotificationLogs.Core;
+
+public interface INotificationLogRepository : IRepositoryBase<TblNotificationLog> { }
