31977712a36df5928efa298530ce269037199421
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:51:50 +0630

    -

--- notification_system.notification/Persistence/Wrapper/IUnitOfWork.cs
+++ notification_system.notification/Persistence/Wrapper/IUnitOfWork.cs
@@ -1,13 +1,12 @@
 ﻿using notification_system.notification.Features.NotificationLogs.Core;
 using notification_system.notification.Features.Otp.Core;
 
-namespace notification_system.notification.Persistence.Wrapper
+namespace notification_system.notification.Persistence.Wrapper;
+
+public interface IUnitOfWork
 {
-    public interface IUnitOfWork
-    {
-        void SaveChanges();
-        Task SaveChangesAsync(CancellationToken cs = default);
-        INotificationLogRepository NotificationLogRepository { get; }
-        IOtpRepository OtpRepository { get; }
-    }
+    void SaveChanges();
+    Task SaveChangesAsync(CancellationToken cs = default);
+    INotificationLogRepository NotificationLogRepository { get; }
+    IOtpRepository OtpRepository { get; }
 }
