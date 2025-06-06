72ccfc8e2619ac601e2eeaec8bbe0a041ad17a33
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:51:26 +0630

    -

--- notification_system.notification/Persistence/Base/IRepositoryBase.cs
+++ notification_system.notification/Persistence/Base/IRepositoryBase.cs
@@ -1,25 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
-namespace notification_system.notification.Persistence.Base
+namespace notification_system.notification.Persistence.Base;
+
+public interface IRepositoryBase<T> where T : class
 {
-    public interface IRepositoryBase<T> where T : class
-    {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression);
-        IQueryable<T> Query(
-            Expression<Func<T, bool>>? expression = null,
-            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>>? order = null
-        );
-        void Add(T entity);
-        void Add(IEnumerable<T> entities);
-        Task AddAsync(T entity, CancellationToken cs = default);
-        Task AddAsync(IEnumerable<T> entities, CancellationToken cs = default);
-        void Update(T entity);
-        void Update(IEnumerable<T> entities);
-        void Delete(T entity);
-        void Delete(IEnumerable<T> entities);
-        void Dispose();
-    }
+    IQueryable<T> GetAll();
+    IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression);
+    IQueryable<T> Query(
+        Expression<Func<T, bool>>? expression = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? order = null
+    );
+    void Add(T entity);
+    void Add(IEnumerable<T> entities);
+    Task AddAsync(T entity, CancellationToken cs = default);
+    Task AddAsync(IEnumerable<T> entities, CancellationToken cs = default);
+    void Update(T entity);
+    void Update(IEnumerable<T> entities);
+    void Delete(T entity);
+    void Delete(IEnumerable<T> entities);
+    void Dispose();
 }
