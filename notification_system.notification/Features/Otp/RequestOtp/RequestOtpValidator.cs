4fd37f9bd236425e30e7d610af545b62c63025c3
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:47:39 +0630

    -

--- notification_system.notification/Features/Otp/RequestOtp/RequestOtpValidator.cs
+++ notification_system.notification/Features/Otp/RequestOtp/RequestOtpValidator.cs
@@ -7,8 +7,11 @@ public class RequestOtpValidator : AbstractValidator<RequestOtpRequest>
     public RequestOtpValidator()
     {
         RuleFor(x => x.Email)
-            .NotEmpty().WithMessage("Email cannot be empty.")
-            .NotNull().WithMessage("Email cannot be null.")
-            .EmailAddress().WithMessage("Email is invailid.");
+            .NotEmpty()
+            .WithMessage("Email cannot be empty.")
+            .NotNull()
+            .WithMessage("Email cannot be null.")
+            .EmailAddress()
+            .WithMessage("Email is invailid.");
     }
 }
