113526e045eb4f419253900fbfc457cc305a727a
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:46:53 +0630

    -

--- notification_system.notification/Features/Otp/RequestOtp/BL_RequestOtp.cs
+++ notification_system.notification/Features/Otp/RequestOtp/BL_RequestOtp.cs
@@ -14,20 +14,25 @@ public class BL_RequestOtp
         _dA_RequestOtp = dA_RequestOtp;
     }
 
-    public async Task<BaseResponse<RequestOtpResponse>> RequestOtp(RequestOtpRequest request, CancellationToken cs = default)
+    public async Task<BaseResponse<RequestOtpResponse>> RequestOtp(
+        RequestOtpRequest request,
+        CancellationToken cs = default
+    )
     {
         BaseResponse<RequestOtpResponse> result;
 
         var validationResult = await _validator.ValidateAsync(request, cs);
         if (!validationResult.IsValid)
         {
-            result = BaseResponse<RequestOtpResponse>.Fail(string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage)));
+            result = BaseResponse<RequestOtpResponse>.Fail(
+                string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage))
+            );
             goto result;
         }
 
         result = await _dA_RequestOtp.RequestOtp(request, cs);
 
-    result:
+        result:
         return result;
     }
 }
