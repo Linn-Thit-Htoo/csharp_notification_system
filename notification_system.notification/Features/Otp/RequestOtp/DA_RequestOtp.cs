77fb605c33507bb631d76a756069e7216d9f406a
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:47:02 +0630

    -

--- notification_system.notification/Features/Otp/RequestOtp/DA_RequestOtp.cs
+++ notification_system.notification/Features/Otp/RequestOtp/DA_RequestOtp.cs
@@ -5,51 +5,50 @@ using notification_system.notification.Persistence.Wrapper;
 using notification_system.notification.Utils;
 using System.Security.Cryptography;
 
-namespace notification_system.notification.Features.Otp.RequestOtp
+namespace notification_system.notification.Features.Otp.RequestOtp;
+
+public class DA_RequestOtp
 {
-    public class DA_RequestOtp
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly AppSetting _setting;
+
+    public DA_RequestOtp(IUnitOfWork unitOfWork, IOptions<AppSetting> setting)
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly AppSetting _setting;
+        _unitOfWork = unitOfWork;
+        _setting = setting.Value;
+    }
 
-        public DA_RequestOtp(IUnitOfWork unitOfWork, IOptions<AppSetting> setting)
+    public async Task<BaseResponse<RequestOtpResponse>> RequestOtp(RequestOtpRequest request, CancellationToken cs = default)
+    {
+        int otpValue = GenerateSixDigitNumber();
+        var item = new TblOtp()
         {
-            _unitOfWork = unitOfWork;
-            _setting = setting.Value;
-        }
+            OtpId = Ulid.NewUlid().ToString(),
+            CreatedAt = DateTime.Now,
+            Email = request.Email,
+            ExpiredAt = DateTime.Now.AddMinutes(_setting.OtpConfig.ExpireInMinutes),
+            IsDeleted = false,
+            OtpValue = otpValue
+        };
 
-        public async Task<BaseResponse<RequestOtpResponse>> RequestOtp(RequestOtpRequest request, CancellationToken cs = default)
+        await _unitOfWork.OtpRepository.AddAsync(item, cs);
+        await _unitOfWork.SaveChangesAsync(cs);
+
+        return BaseResponse<RequestOtpResponse>.Success(new RequestOtpResponse
         {
-            int otpValue = GenerateSixDigitNumber();
-            var item = new TblOtp()
-            {
-                OtpId = Ulid.NewUlid().ToString(),
-                CreatedAt = DateTime.Now,
-                Email = request.Email,
-                ExpiredAt = DateTime.Now.AddMinutes(_setting.OtpConfig.ExpireInMinutes),
-                IsDeleted = false,
-                OtpValue = otpValue
-            };
+            ExpiredAt = item.ExpiredAt,
+            Otp = otpValue,
+        });
+    }
 
-            await _unitOfWork.OtpRepository.AddAsync(item, cs);
-            await _unitOfWork.SaveChangesAsync(cs);
-
-            return BaseResponse<RequestOtpResponse>.Success(new RequestOtpResponse
-            {
-                ExpiredAt = item.ExpiredAt,
-                Otp = otpValue,
-            });
-        }
-
-        private int GenerateSixDigitNumber()
+    private int GenerateSixDigitNumber()
+    {
+        using (var rng = RandomNumberGenerator.Create())
         {
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                byte[] bytes = new byte[4];
-                rng.GetBytes(bytes);
-                int randomValue = BitConverter.ToInt32(bytes, 0) & int.MaxValue;
-                return (randomValue % 900000) + 100000;
-            }
+            byte[] bytes = new byte[4];
+            rng.GetBytes(bytes);
+            int randomValue = BitConverter.ToInt32(bytes, 0) & int.MaxValue;
+            return (randomValue % 900000) + 100000;
         }
     }
 }
