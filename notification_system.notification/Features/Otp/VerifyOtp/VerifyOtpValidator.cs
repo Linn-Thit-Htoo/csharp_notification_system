2e5c2c4670033ebef0b1e7a463a6e90fe91611c1
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:48:34 +0630

    -

--- notification_system.notification/Features/Otp/VerifyOtp/VerifyOtpValidator.cs
+++ notification_system.notification/Features/Otp/VerifyOtp/VerifyOtpValidator.cs
@@ -1,22 +1,21 @@
 ﻿using FluentValidation;
 
-namespace notification_system.notification.Features.Otp.VerifyOtp
+namespace notification_system.notification.Features.Otp.VerifyOtp;
+
+public class VerifyOtpValidator : AbstractValidator<VerifyOtpRequest>
 {
-    public class VerifyOtpValidator : AbstractValidator<VerifyOtpRequest>
+    public VerifyOtpValidator()
     {
-        public VerifyOtpValidator()
-        {
-            RuleFor(x => x.RefId)
-                .NotEmpty().WithMessage("Ref Id cannot be empty.")
-                .NotNull().WithMessage("Ref Id cannot be null.");
+        RuleFor(x => x.RefId)
+            .NotEmpty().WithMessage("Ref Id cannot be empty.")
+            .NotNull().WithMessage("Ref Id cannot be null.");
 
-            RuleFor(x => x.OtpValue)
-                .GreaterThan(0).WithMessage("Invalid Otp.");
+        RuleFor(x => x.OtpValue)
+            .GreaterThan(0).WithMessage("Invalid Otp.");
 
-            RuleFor(x => x.Email)
-                .NotEmpty().WithMessage("Email cannot be empty.")
-                .NotNull().WithMessage("Email cannot be null.")
-                .EmailAddress().WithMessage("Email is invailid.");
-        }
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email cannot be empty.")
+            .NotNull().WithMessage("Email cannot be null.")
+            .EmailAddress().WithMessage("Email is invailid.");
     }
 }
