a98e7ab61d0315b9523a4991e534effaa4317b46
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:48:38 +0630

    -

--- notification_system.notification/Features/Otp/VerifyOtp/VerifyOtpValidator.cs
+++ notification_system.notification/Features/Otp/VerifyOtp/VerifyOtpValidator.cs
@@ -7,15 +7,19 @@ public class VerifyOtpValidator : AbstractValidator<VerifyOtpRequest>
     public VerifyOtpValidator()
     {
         RuleFor(x => x.RefId)
-            .NotEmpty().WithMessage("Ref Id cannot be empty.")
-            .NotNull().WithMessage("Ref Id cannot be null.");
+            .NotEmpty()
+            .WithMessage("Ref Id cannot be empty.")
+            .NotNull()
+            .WithMessage("Ref Id cannot be null.");
 
-        RuleFor(x => x.OtpValue)
-            .GreaterThan(0).WithMessage("Invalid Otp.");
+        RuleFor(x => x.OtpValue).GreaterThan(0).WithMessage("Invalid Otp.");
 
         RuleFor(x => x.Email)
-            .NotEmpty().WithMessage("Email cannot be empty.")
-            .NotNull().WithMessage("Email cannot be null.")
-            .EmailAddress().WithMessage("Email is invailid.");
+            .NotEmpty()
+            .WithMessage("Email cannot be empty.")
+            .NotNull()
+            .WithMessage("Email cannot be null.")
+            .EmailAddress()
+            .WithMessage("Email is invailid.");
     }
 }
