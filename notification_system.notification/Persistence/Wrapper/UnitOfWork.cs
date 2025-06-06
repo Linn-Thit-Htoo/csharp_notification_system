d3b7b5c1aeb8fd8c4e2432912b0705c853240a74
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:51:58 +0630

    -

--- notification_system.notification/Persistence/Wrapper/UnitOfWork.cs
+++ notification_system.notification/Persistence/Wrapper/UnitOfWork.cs
@@ -5,103 +5,102 @@ using notification_system.notification.Features.Otp.Core;
 using System.Reflection;
 using System.Security.Claims;
 
-namespace notification_system.notification.Persistence.Wrapper
+namespace notification_system.notification.Persistence.Wrapper;
+
+public class UnitOfWork : IUnitOfWork
 {
-    public class UnitOfWork : IUnitOfWork
+    internal readonly DbContext _context;
+    internal readonly string? _currentUser = "SYSTEM";
+    internal readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UnitOfWork(IHttpContextAccessor httpContextAccessor, NotificationDbContext dbContext)
     {
-        internal readonly DbContext _context;
-        internal readonly string? _currentUser = "SYSTEM";
-        internal readonly IHttpContextAccessor _httpContextAccessor;
+        _httpContextAccessor = httpContextAccessor;
+        _context = dbContext;
 
-        public UnitOfWork(IHttpContextAccessor httpContextAccessor, NotificationDbContext dbContext)
+        if (_httpContextAccessor.HttpContext is not null)
         {
-            _httpContextAccessor = httpContextAccessor;
-            _context = dbContext;
-
-            if (_httpContextAccessor.HttpContext is not null)
+            if (_httpContextAccessor.HttpContext.User.Identity!.IsAuthenticated)
             {
-                if (_httpContextAccessor.HttpContext.User.Identity!.IsAuthenticated)
-                {
-                    _currentUser = _httpContextAccessor
-                        .HttpContext.User.Identities.FirstOrDefault()!
-                        .Claims.Where(x => x.Type == ClaimTypes.NameIdentifier)
-                        .FirstOrDefault()!
-                        .Value;
-                }
+                _currentUser = _httpContextAccessor
+                    .HttpContext.User.Identities.FirstOrDefault()!
+                    .Claims.Where(x => x.Type == ClaimTypes.NameIdentifier)
+                    .FirstOrDefault()!
+                    .Value;
             }
-
-            NotificationLogRepository = new NotificationLogRepository(_context);
-            OtpRepository = new OtpRepository(_context);
         }
 
-        public void SaveChanges()
-        {
-            var modifiedEntries = _context
-                .ChangeTracker.Entries()
-                .Where(x => x.State == EntityState.Modified || x.State == EntityState.Added)
-                .ToList();
-
-            foreach (var entry in modifiedEntries)
-            {
-                Type type = entry.Entity.GetType();
-
-                if (entry.State == EntityState.Added)
-                {
-                    PropertyInfo createdBy = type.GetProperty("CreatedBy")!;
-                    createdBy?.SetValue(entry.Entity, _currentUser);
-
-                    PropertyInfo createdDate = type.GetProperty("CreatedAt")!;
-                    createdDate?.SetValue(entry.Entity, DateTime.Now);
-                }
-
-                if (entry.State == EntityState.Modified)
-                {
-                    PropertyInfo modifiedBy = type.GetProperty("ModifiedBy")!;
-                    modifiedBy?.SetValue(entry.Entity, _currentUser);
-
-                    PropertyInfo modifiedAt = type.GetProperty("ModifiedAt")!;
-                    modifiedAt?.SetValue(entry.Entity, DateTime.Now);
-                }
-            }
-
-            _context.SaveChanges();
-        }
-
-        public async Task SaveChangesAsync(CancellationToken cs = default)
-        {
-            var modifiedEntries = _context
-                .ChangeTracker.Entries()
-                .Where(x => x.State == EntityState.Modified || x.State == EntityState.Added)
-                .ToList();
-
-            foreach (var entry in modifiedEntries)
-            {
-                Type type = entry.Entity.GetType();
-
-                if (entry.State == EntityState.Added)
-                {
-                    PropertyInfo createdBy = type.GetProperty("CreatedBy")!;
-                    createdBy?.SetValue(entry.Entity, _currentUser);
-
-                    PropertyInfo createdDate = type.GetProperty("CreatedAt")!;
-                    createdDate?.SetValue(entry.Entity, DateTime.Now);
-                }
-
-                if (entry.State == EntityState.Modified)
-                {
-                    PropertyInfo modifiedBy = type.GetProperty("ModifiedBy")!;
-                    modifiedBy?.SetValue(entry.Entity, _currentUser);
-
-                    PropertyInfo modifiedAt = type.GetProperty("ModifiedAt")!;
-                    modifiedAt?.SetValue(entry.Entity, DateTime.Now);
-                }
-            }
-
-            await _context.SaveChangesAsync(cs);
-        }
-
-        public INotificationLogRepository NotificationLogRepository { get; set; }
-
-        public IOtpRepository OtpRepository { get; set; }
+        NotificationLogRepository = new NotificationLogRepository(_context);
+        OtpRepository = new OtpRepository(_context);
     }
+
+    public void SaveChanges()
+    {
+        var modifiedEntries = _context
+            .ChangeTracker.Entries()
+            .Where(x => x.State == EntityState.Modified || x.State == EntityState.Added)
+            .ToList();
+
+        foreach (var entry in modifiedEntries)
+        {
+            Type type = entry.Entity.GetType();
+
+            if (entry.State == EntityState.Added)
+            {
+                PropertyInfo createdBy = type.GetProperty("CreatedBy")!;
+                createdBy?.SetValue(entry.Entity, _currentUser);
+
+                PropertyInfo createdDate = type.GetProperty("CreatedAt")!;
+                createdDate?.SetValue(entry.Entity, DateTime.Now);
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                PropertyInfo modifiedBy = type.GetProperty("ModifiedBy")!;
+                modifiedBy?.SetValue(entry.Entity, _currentUser);
+
+                PropertyInfo modifiedAt = type.GetProperty("ModifiedAt")!;
+                modifiedAt?.SetValue(entry.Entity, DateTime.Now);
+            }
+        }
+
+        _context.SaveChanges();
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cs = default)
+    {
+        var modifiedEntries = _context
+            .ChangeTracker.Entries()
+            .Where(x => x.State == EntityState.Modified || x.State == EntityState.Added)
+            .ToList();
+
+        foreach (var entry in modifiedEntries)
+        {
+            Type type = entry.Entity.GetType();
+
+            if (entry.State == EntityState.Added)
+            {
+                PropertyInfo createdBy = type.GetProperty("CreatedBy")!;
+                createdBy?.SetValue(entry.Entity, _currentUser);
+
+                PropertyInfo createdDate = type.GetProperty("CreatedAt")!;
+                createdDate?.SetValue(entry.Entity, DateTime.Now);
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                PropertyInfo modifiedBy = type.GetProperty("ModifiedBy")!;
+                modifiedBy?.SetValue(entry.Entity, _currentUser);
+
+                PropertyInfo modifiedAt = type.GetProperty("ModifiedAt")!;
+                modifiedAt?.SetValue(entry.Entity, DateTime.Now);
+            }
+        }
+
+        await _context.SaveChangesAsync(cs);
+    }
+
+    public INotificationLogRepository NotificationLogRepository { get; set; }
+
+    public IOtpRepository OtpRepository { get; set; }
 }
