b03e1f7df8e28bffedd868da60d0e2180159897f
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:49:40 +0630

    -

--- notification_system.notification/Features/SMS/Core/SMSController.cs
+++ notification_system.notification/Features/SMS/Core/SMSController.cs
@@ -2,32 +2,31 @@
 using Microsoft.AspNetCore.Mvc;
 using notification_system.notification.Features.SMS.SendSMS;
 
-namespace notification_system.notification.Features.SMS.Core
+namespace notification_system.notification.Features.SMS.Core;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class SMSController : BaseController
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    public class SMSController : BaseController
+    private readonly BL_SendSMS _bL_SendSMS;
+
+    public SMSController(BL_SendSMS bL_SendSMS)
     {
-        private readonly BL_SendSMS _bL_SendSMS;
+        _bL_SendSMS = bL_SendSMS;
+    }
 
-        public SMSController(BL_SendSMS bL_SendSMS)
-        {
-            _bL_SendSMS = bL_SendSMS;
-        }
+    [HttpPost("SendSingleSMS")]
+    public async Task<IActionResult> SendSingleSMS(SendSingleSMSRequest request, CancellationToken cs)
+    {
+        var result = await _bL_SendSMS.SendSingleSMSAsync(request, cs);
+        return Content(result);
+    }
 
-        [HttpPost("SendSingleSMS")]
-        public async Task<IActionResult> SendSingleSMS(SendSingleSMSRequest request, CancellationToken cs)
-        {
-            var result = await _bL_SendSMS.SendSingleSMSAsync(request, cs);
-            return Content(result);
-        }
-
-        [HttpPost("SendMultipleSMS")]
-        [Obsolete]
-        public async Task<IActionResult> SendMultipleSMS(SendMultipleSMSRequest request, CancellationToken cs)
-        {
-            var result = await _bL_SendSMS.SendMultipleSMSAsync(request, cs);
-            return Content(result);
-        }
+    [HttpPost("SendMultipleSMS")]
+    [Obsolete]
+    public async Task<IActionResult> SendMultipleSMS(SendMultipleSMSRequest request, CancellationToken cs)
+    {
+        var result = await _bL_SendSMS.SendMultipleSMSAsync(request, cs);
+        return Content(result);
     }
 }
