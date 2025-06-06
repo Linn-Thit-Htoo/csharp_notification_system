edbde30d912b0705cf8571d4dc97ab7ae7a6d354
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:49:42 +0630

    -

--- notification_system.notification/Features/SMS/Core/SMSController.cs
+++ notification_system.notification/Features/SMS/Core/SMSController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using notification_system.notification.Features.SMS.SendSMS;
 
 namespace notification_system.notification.Features.SMS.Core;
