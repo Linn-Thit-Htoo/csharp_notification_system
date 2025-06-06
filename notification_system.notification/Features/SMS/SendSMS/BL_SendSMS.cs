dd481dc73df237ffc9094fae40ad99bdefc85f5f
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:49:54 +0630

    -

--- notification_system.notification/Features/SMS/SendSMS/BL_SendSMS.cs
+++ notification_system.notification/Features/SMS/SendSMS/BL_SendSMS.cs
@@ -2,61 +2,60 @@
 using notification_system.notification.Services.SMSServices;
 using notification_system.notification.Utils;
 
-namespace notification_system.notification.Features.SMS.SendSMS
+namespace notification_system.notification.Features.SMS.SendSMS;
+
+public class BL_SendSMS
 {
-    public class BL_SendSMS
+    private readonly ITwilioService _twilioService;
+
+    public BL_SendSMS(ITwilioService twilioService)
     {
-        private readonly ITwilioService _twilioService;
+        _twilioService = twilioService;
+    }
 
-        public BL_SendSMS(ITwilioService twilioService)
+    public async Task<BaseResponse<SendSMSResponse>> SendSingleSMSAsync(SendSingleSMSRequest request, CancellationToken cs = default)
+    {
+        BaseResponse<SendSMSResponse> result;
+
+        if (request.ToPhoneNumber.IsNullOrEmpty())
         {
-            _twilioService = twilioService;
+            result = BaseResponse<SendSMSResponse>.Fail("To Phone Number cannot be empty.");
+            goto result;
         }
 
-        public async Task<BaseResponse<SendSMSResponse>> SendSingleSMSAsync(SendSingleSMSRequest request, CancellationToken cs = default)
+        if (request.Messasge.IsNullOrEmpty())
         {
-            BaseResponse<SendSMSResponse> result;
-
-            if (request.ToPhoneNumber.IsNullOrEmpty())
-            {
-                result = BaseResponse<SendSMSResponse>.Fail("To Phone Number cannot be empty.");
-                goto result;
-            }
-
-            if (request.Messasge.IsNullOrEmpty())
-            {
-                result = BaseResponse<SendSMSResponse>.Fail("Message cannot be empty.");
-                goto result;
-            }
-
-            await _twilioService.SendSingleSMSAsync(request, cs);
-            result = BaseResponse<SendSMSResponse>.Success();
-
-        result:
-            return result;
+            result = BaseResponse<SendSMSResponse>.Fail("Message cannot be empty.");
+            goto result;
         }
 
-        public async Task<BaseResponse<SendSMSResponse>> SendMultipleSMSAsync(SendMultipleSMSRequest request, CancellationToken cs = default)
+        await _twilioService.SendSingleSMSAsync(request, cs);
+        result = BaseResponse<SendSMSResponse>.Success();
+
+    result:
+        return result;
+    }
+
+    public async Task<BaseResponse<SendSMSResponse>> SendMultipleSMSAsync(SendMultipleSMSRequest request, CancellationToken cs = default)
+    {
+        BaseResponse<SendSMSResponse> result;
+
+        if (request.ToPhoneNumbers is null || request.ToPhoneNumbers.Count <= 0)
         {
-            BaseResponse<SendSMSResponse> result;
-
-            if (request.ToPhoneNumbers is null || request.ToPhoneNumbers.Count <= 0)
-            {
-                result = BaseResponse<SendSMSResponse>.Fail("To Phone Number cannot be empty.");
-                goto result;
-            }
-
-            if (request.Messasge.IsNullOrEmpty())
-            {
-                result = BaseResponse<SendSMSResponse>.Fail("Message cannot be empty.");
-                goto result;
-            }
-
-            await _twilioService.SendMultipleSMSAsync(request, cs);
-            result = BaseResponse<SendSMSResponse>.Success();
-
-        result:
-            return result;
+            result = BaseResponse<SendSMSResponse>.Fail("To Phone Number cannot be empty.");
+            goto result;
         }
+
+        if (request.Messasge.IsNullOrEmpty())
+        {
+            result = BaseResponse<SendSMSResponse>.Fail("Message cannot be empty.");
+            goto result;
+        }
+
+        await _twilioService.SendMultipleSMSAsync(request, cs);
+        result = BaseResponse<SendSMSResponse>.Success();
+
+    result:
+        return result;
     }
 }
