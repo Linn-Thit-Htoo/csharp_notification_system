cdd610b435892f2e596f31c9a08ac202d9123a22
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-07 01:19:03 +0630

    -

--- notification_system.notification/Services/Kafka/PushNotiConsumerService.cs
+++ notification_system.notification/Services/Kafka/PushNotiConsumerService.cs
@@ -5,54 +5,53 @@ using notification_system.notification.Extensions;
 using notification_system.notification.Features.PushNoti;
 using notification_system.notification.Services.PushNoti;
 
-namespace notification_system.notification.Services.Kafka
+namespace notification_system.notification.Services.Kafka;
+
+public class PushNotiConsumerService : BackgroundService
 {
-    public class PushNotiConsumerService : BackgroundService
+    private readonly IConsumer<Ignore, string> _consumer;
+    private readonly ILogger<MultipleSMSConsumerService> _logger;
+    private readonly AppSetting _appSetting;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public PushNotiConsumerService(ILogger<MultipleSMSConsumerService> logger, IOptions<AppSetting> setting, IServiceScopeFactory serviceScopeFactory)
     {
-        private readonly IConsumer<Ignore, string> _consumer;
-        private readonly ILogger<MultipleSMSConsumerService> _logger;
-        private readonly AppSetting _appSetting;
-        private readonly IServiceScopeFactory _serviceScopeFactory;
+        _logger = logger;
+        _appSetting = setting.Value;
 
-        public PushNotiConsumerService(ILogger<MultipleSMSConsumerService> logger, IOptions<AppSetting> setting, IServiceScopeFactory serviceScopeFactory)
+        var consumerConfig = new ConsumerConfig
         {
-            _logger = logger;
-            _appSetting = setting.Value;
+            BootstrapServers = _appSetting.Kafka.BootstrapServers,
+            GroupId = _appSetting.Kafka.PushNotification.GroupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
 
-            var consumerConfig = new ConsumerConfig
-            {
-                BootstrapServers = _appSetting.Kafka.BootstrapServers,
-                GroupId = _appSetting.Kafka.PushNotification.GroupId,
-                AutoOffsetReset = AutoOffsetReset.Earliest
-            };
+        _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+        _serviceScopeFactory = serviceScopeFactory;
+        _logger = logger;
+    }
 
-            _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
-            _serviceScopeFactory = serviceScopeFactory;
-            _logger = logger;
-        }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Extension.EnsureTopicExistsAsync(_appSetting.Kafka.BootstrapServers, _appSetting.Kafka.PushNotification.Topic);
+        _consumer.Subscribe(_appSetting.Kafka.PushNotification.Topic);
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            await Extension.EnsureTopicExistsAsync(_appSetting.Kafka.BootstrapServers, _appSetting.Kafka.PushNotification.Topic);
-            _consumer.Subscribe(_appSetting.Kafka.PushNotification.Topic);
-
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                try
-                {
-                    var scope = _serviceScopeFactory.CreateScope();
-                    var pushNotiService = scope.ServiceProvider.GetRequiredService<IPushNotiService>();
-                    var consumeResult = _consumer.Consume(stoppingToken);
-                    var message = consumeResult.Message.Value;
-                    var request = message.ToObject<PushNotiRequest>();
+                var scope = _serviceScopeFactory.CreateScope();
+                var pushNotiService = scope.ServiceProvider.GetRequiredService<IPushNotiService>();
+                var consumeResult = _consumer.Consume(stoppingToken);
+                var message = consumeResult.Message.Value;
+                var request = message.ToObject<PushNotiRequest>();
 
-                    await pushNotiService.PushNotiAsync(request, stoppingToken);
-                    _logger.LogInformation($"Received Push Noti Consumer: {message}");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError($"Error processing Push Noti Consumer: {ex.Message}");
-                }
+                await pushNotiService.PushNotiAsync(request, stoppingToken);
+                _logger.LogInformation($"Received Push Noti Consumer: {message}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error processing Push Noti Consumer: {ex.Message}");
             }
         }
     }
