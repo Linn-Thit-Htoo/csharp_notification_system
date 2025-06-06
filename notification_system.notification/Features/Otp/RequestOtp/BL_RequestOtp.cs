468295f503ab804b03e503027de58cdfc50920a4
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:46:50 +0630

    -

--- notification_system.notification/Features/Otp/RequestOtp/BL_RequestOtp.cs
+++ notification_system.notification/Features/Otp/RequestOtp/BL_RequestOtp.cs
@@ -1,34 +1,33 @@
 ﻿using FluentValidation;
 using notification_system.notification.Utils;
 
-namespace notification_system.notification.Features.Otp.RequestOtp
+namespace notification_system.notification.Features.Otp.RequestOtp;
+
+public class BL_RequestOtp
 {
-    public class BL_RequestOtp
+    private readonly IValidator<RequestOtpRequest> _validator;
+    private readonly DA_RequestOtp _dA_RequestOtp;
+
+    public BL_RequestOtp(IValidator<RequestOtpRequest> validator, DA_RequestOtp dA_RequestOtp)
     {
-        private readonly IValidator<RequestOtpRequest> _validator;
-        private readonly DA_RequestOtp _dA_RequestOtp;
+        _validator = validator;
+        _dA_RequestOtp = dA_RequestOtp;
+    }
 
-        public BL_RequestOtp(IValidator<RequestOtpRequest> validator, DA_RequestOtp dA_RequestOtp)
+    public async Task<BaseResponse<RequestOtpResponse>> RequestOtp(RequestOtpRequest request, CancellationToken cs = default)
+    {
+        BaseResponse<RequestOtpResponse> result;
+
+        var validationResult = await _validator.ValidateAsync(request, cs);
+        if (!validationResult.IsValid)
         {
-            _validator = validator;
-            _dA_RequestOtp = dA_RequestOtp;
+            result = BaseResponse<RequestOtpResponse>.Fail(string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage)));
+            goto result;
         }
 
-        public async Task<BaseResponse<RequestOtpResponse>> RequestOtp(RequestOtpRequest request, CancellationToken cs = default)
-        {
-            BaseResponse<RequestOtpResponse> result;
+        result = await _dA_RequestOtp.RequestOtp(request, cs);
 
-            var validationResult = await _validator.ValidateAsync(request, cs);
-            if (!validationResult.IsValid)
-            {
-                result = BaseResponse<RequestOtpResponse>.Fail(string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage)));
-                goto result;
-            }
-
-            result = await _dA_RequestOtp.RequestOtp(request, cs);
-
-        result:
-            return result;
-        }
+    result:
+        return result;
     }
 }
