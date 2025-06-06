8a9d44bfb43ae89a490b33758e467b44c348e1d7
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:49:28 +0630

    -

--- notification_system.notification/Features/PushNoti/PushNotiValidator.cs
+++ notification_system.notification/Features/PushNoti/PushNotiValidator.cs
@@ -7,15 +7,21 @@ public class PushNotiValidator : AbstractValidator<PushNotiRequest>
     public PushNotiValidator()
     {
         RuleFor(x => x.DeviceToken)
-            .NotEmpty().WithMessage("Device Token cannot be empty.")
-            .NotNull().WithMessage("Device Token cannot be null.");
+            .NotEmpty()
+            .WithMessage("Device Token cannot be empty.")
+            .NotNull()
+            .WithMessage("Device Token cannot be null.");
 
         RuleFor(x => x.Title)
-            .NotEmpty().WithMessage("Title cannot be empty.")
-            .NotNull().WithMessage("Title cannot be null.");
+            .NotEmpty()
+            .WithMessage("Title cannot be empty.")
+            .NotNull()
+            .WithMessage("Title cannot be null.");
 
         RuleFor(x => x.Body)
-            .NotEmpty().WithMessage("Body cannot be empty.")
-            .NotNull().WithMessage("Body cannot be null.");
+            .NotEmpty()
+            .WithMessage("Body cannot be empty.")
+            .NotNull()
+            .WithMessage("Body cannot be null.");
     }
 }
