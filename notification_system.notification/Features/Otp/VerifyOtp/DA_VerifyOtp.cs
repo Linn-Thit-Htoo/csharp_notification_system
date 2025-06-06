55df5babe5aa17aa1520bcd6e5a6c051967d0b8b
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:47:57 +0630

    -

--- notification_system.notification/Features/Otp/VerifyOtp/DA_VerifyOtp.cs
+++ notification_system.notification/Features/Otp/VerifyOtp/DA_VerifyOtp.cs
@@ -2,38 +2,37 @@
 using notification_system.notification.Persistence.Wrapper;
 using notification_system.notification.Utils;
 
-namespace notification_system.notification.Features.Otp.VerifyOtp
+namespace notification_system.notification.Features.Otp.VerifyOtp;
+
+public class DA_VerifyOtp
 {
-    public class DA_VerifyOtp
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DA_VerifyOtp(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
+        _unitOfWork = unitOfWork;
+    }
 
-        public DA_VerifyOtp(IUnitOfWork unitOfWork)
+    public async Task<BaseResponse<VerifyOtpResponse>> VerifyOtpAsync(VerifyOtpRequest request, CancellationToken cs = default)
+    {
+        BaseResponse<VerifyOtpResponse> result;
+
+        var item = await _unitOfWork.OtpRepository
+            .GetByCondition(x => x.OtpId == request.RefId && x.OtpValue == request.OtpValue && x.ExpiredAt > DateTime.Now && !x.IsDeleted)
+            .SingleOrDefaultAsync(cs);
+        if (item is null)
         {
-            _unitOfWork = unitOfWork;
+            result = BaseResponse<VerifyOtpResponse>.NotFound("No data found.");
+            goto result;
         }
 
-        public async Task<BaseResponse<VerifyOtpResponse>> VerifyOtpAsync(VerifyOtpRequest request, CancellationToken cs = default)
-        {
-            BaseResponse<VerifyOtpResponse> result;
+        item.IsDeleted = true;
+        _unitOfWork.OtpRepository.Update(item);
+        await _unitOfWork.SaveChangesAsync(cs);
 
-            var item = await _unitOfWork.OtpRepository
-                .GetByCondition(x => x.OtpId == request.RefId && x.OtpValue == request.OtpValue && x.ExpiredAt > DateTime.Now && !x.IsDeleted)
-                .SingleOrDefaultAsync(cs);
-            if (item is null)
-            {
-                result = BaseResponse<VerifyOtpResponse>.NotFound("No data found.");
-                goto result;
-            }
+        result = BaseResponse<VerifyOtpResponse>.Success();
 
-            item.IsDeleted = true;
-            _unitOfWork.OtpRepository.Update(item);
-            await _unitOfWork.SaveChangesAsync(cs);
-
-            result = BaseResponse<VerifyOtpResponse>.Success();
-
-        result:
-            return result;
-        }
+    result:
+        return result;
     }
 }
