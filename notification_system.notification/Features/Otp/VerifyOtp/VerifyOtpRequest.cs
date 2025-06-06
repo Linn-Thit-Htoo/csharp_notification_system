26cd0746be1080d0bba0dc4a5c29a0bdc6e7ebe1
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:48:18 +0630

    -

--- notification_system.notification/Features/Otp/VerifyOtp/VerifyOtpRequest.cs
+++ notification_system.notification/Features/Otp/VerifyOtp/VerifyOtpRequest.cs
@@ -1,9 +1,8 @@
-﻿namespace notification_system.notification.Features.Otp.VerifyOtp
+﻿namespace notification_system.notification.Features.Otp.VerifyOtp;
+
+public class VerifyOtpRequest
 {
-    public class VerifyOtpRequest
-    {
-        public string RefId { get; set; }
-        public int OtpValue { get; set; }
-        public string Email { get; set; }
-    }
+    public string RefId { get; set; }
+    public int OtpValue { get; set; }
+    public string Email { get; set; }
 }
