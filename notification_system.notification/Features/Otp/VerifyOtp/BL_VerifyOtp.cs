e108b04e9ac9038db45f20aaaeb7143e67268954
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-06 14:47:46 +0630

    -

--- notification_system.notification/Features/Otp/VerifyOtp/BL_VerifyOtp.cs
+++ notification_system.notification/Features/Otp/VerifyOtp/BL_VerifyOtp.cs
@@ -1,34 +1,33 @@
 ﻿using FluentValidation;
 using notification_system.notification.Utils;
 
-namespace notification_system.notification.Features.Otp.VerifyOtp
+namespace notification_system.notification.Features.Otp.VerifyOtp;
+
+public class BL_VerifyOtp
 {
-    public class BL_VerifyOtp
+    private readonly DA_VerifyOtp _dA_VerifyOtp;
+    private readonly IValidator<VerifyOtpRequest> _validator;
+
+    public BL_VerifyOtp(DA_VerifyOtp dA_VerifyOtp, IValidator<VerifyOtpRequest> validator)
     {
-        private readonly DA_VerifyOtp _dA_VerifyOtp;
-        private readonly IValidator<VerifyOtpRequest> _validator;
+        _dA_VerifyOtp = dA_VerifyOtp;
+        _validator = validator;
+    }
 
-        public BL_VerifyOtp(DA_VerifyOtp dA_VerifyOtp, IValidator<VerifyOtpRequest> validator)
+    public async Task<BaseResponse<VerifyOtpResponse>> VerifyOtpAsync(VerifyOtpRequest request, CancellationToken cs = default)
+    {
+        BaseResponse<VerifyOtpResponse> result;
+
+        var validationResult = await _validator.ValidateAsync(request, cs);
+        if (!validationResult.IsValid)
         {
-            _dA_VerifyOtp = dA_VerifyOtp;
-            _validator = validator;
+            result = BaseResponse<VerifyOtpResponse>.Fail(string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage)));
+            goto result;
         }
 
-        public async Task<BaseResponse<VerifyOtpResponse>> VerifyOtpAsync(VerifyOtpRequest request, CancellationToken cs = default)
-        {
-            BaseResponse<VerifyOtpResponse> result;
+        result = await _dA_VerifyOtp.VerifyOtpAsync(request, cs);
 
-            var validationResult = await _validator.ValidateAsync(request, cs);
-            if (!validationResult.IsValid)
-            {
-                result = BaseResponse<VerifyOtpResponse>.Fail(string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage)));
-                goto result;
-            }
-
-            result = await _dA_VerifyOtp.VerifyOtpAsync(request, cs);
-
-        result:
-            return result;
-        }
+    result:
+        return result;
     }
 }
