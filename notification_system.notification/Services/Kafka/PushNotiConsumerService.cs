56794ad18b0ab52800116ca7c3f0eb8e08e6a70a
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-07 01:18:55 +0630

    -

--- notification_system.notification/Services/Kafka/PushNotiConsumerService.cs
+++ notification_system.notification/Services/Kafka/PushNotiConsumerService.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using notification_system.notification.Configurations;
 using notification_system.notification.Extensions;
-using notification_system.notification.Features.Email.SendEmail;
 using notification_system.notification.Features.PushNoti;
 using notification_system.notification.Services.PushNoti;
 
