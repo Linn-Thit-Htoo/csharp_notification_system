71219b6553c5cf34eeb3040315519f894819954a
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-07 01:18:43 +0630

    -

--- notification_system.notification/Services/Kafka/MultipleSMSConsumerService.cs
+++ notification_system.notification/Services/Kafka/MultipleSMSConsumerService.cs
@@ -6,53 +6,52 @@ using notification_system.notification.Features.SMS.SendSMS;
 using notification_system.notification.Services.SMSServices;
 using static notification_system.notification.Extensions.Extension;
 
-namespace notification_system.notification.Services.Kafka
+namespace notification_system.notification.Services.Kafka;
+
+public class MultipleSMSConsumerService : BackgroundService
 {
-    public class MultipleSMSConsumerService : BackgroundService
+    private readonly IConsumer<Ignore, string> _consumer;
+    private readonly ILogger<MultipleSMSConsumerService> _logger;
+    private readonly AppSetting _appSetting;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public MultipleSMSConsumerService(ILogger<MultipleSMSConsumerService> logger, IOptions<AppSetting> setting, IServiceScopeFactory serviceScopeFactory)
     {
-        private readonly IConsumer<Ignore, string> _consumer;
-        private readonly ILogger<MultipleSMSConsumerService> _logger;
-        private readonly AppSetting _appSetting;
-        private readonly IServiceScopeFactory _serviceScopeFactory;
+        _logger = logger;
+        _appSetting = setting.Value;
 
-        public MultipleSMSConsumerService(ILogger<MultipleSMSConsumerService> logger, IOptions<AppSetting> setting, IServiceScopeFactory serviceScopeFactory)
+        var consumerConfig = new ConsumerConfig
         {
-            _logger = logger;
-            _appSetting = setting.Value;
+            BootstrapServers = _appSetting.Kafka.BootstrapServers,
+            GroupId = _appSetting.Kafka.SMS.MultipleSMS.GroupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
 
-            var consumerConfig = new ConsumerConfig
-            {
-                BootstrapServers = _appSetting.Kafka.BootstrapServers,
-                GroupId = _appSetting.Kafka.SMS.MultipleSMS.GroupId,
-                AutoOffsetReset = AutoOffsetReset.Earliest
-            };
+        _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+        _serviceScopeFactory = serviceScopeFactory;
+    }
 
-            _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
-            _serviceScopeFactory = serviceScopeFactory;
-        }
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Extension.EnsureTopicExistsAsync(_appSetting.Kafka.BootstrapServers, _appSetting.Kafka.SMS.MultipleSMS.Topic);
+        _consumer.Subscribe(_appSetting.Kafka.SMS.MultipleSMS.Topic);
 
-        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            await Extension.EnsureTopicExistsAsync(_appSetting.Kafka.BootstrapServers, _appSetting.Kafka.SMS.MultipleSMS.Topic);
-            _consumer.Subscribe(_appSetting.Kafka.SMS.MultipleSMS.Topic);
-
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                try
-                {
-                    var scope = _serviceScopeFactory.CreateScope();
-                    var smsService = scope.ServiceProvider.GetRequiredService<ITwilioService>();
-                    var consumeResult = _consumer.Consume(stoppingToken);
-                    var message = consumeResult.Message.Value;
-                    var request = message.ToObject<SendMultipleSMSRequest>();
+                var scope = _serviceScopeFactory.CreateScope();
+                var smsService = scope.ServiceProvider.GetRequiredService<ITwilioService>();
+                var consumeResult = _consumer.Consume(stoppingToken);
+                var message = consumeResult.Message.Value;
+                var request = message.ToObject<SendMultipleSMSRequest>();
 
-                    await smsService.SendMultipleSMSAsync(request, stoppingToken);
-                    _logger.LogInformation($"Received Single SMS Consumer: {message}");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError($"Error processing Single SMS Consumer: {ex.Message}");
-                }
+                await smsService.SendMultipleSMSAsync(request, stoppingToken);
+                _logger.LogInformation($"Received Single SMS Consumer: {message}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error processing Single SMS Consumer: {ex.Message}");
             }
         }
     }
