54a624af57d99a8d618843835357a0136103e2bf
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-07 23:34:17 +0630

    -

--- notification_system.notification/Services/RabbitMQ/RabbitMQService.cs
+++ notification_system.notification/Services/RabbitMQ/RabbitMQService.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using notification_system.notification.Configurations;
 using notification_system.notification.Extensions;
 using notification_system.notification.Features.Email.SendEmail;
