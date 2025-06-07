7c15f1382dc7d7e63fd80027173c1239e63b5d0b
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-07 22:20:49 +0630

    -

--- notification_system.notification/Services/PushNoti/PushNotiService.cs
+++ notification_system.notification/Services/PushNoti/PushNotiService.cs
@@ -6,75 +6,74 @@ using notification_system.notification.Features.PushNoti;
 using notification_system.notification.Persistence.Wrapper;
 using notification_system.notification.Utils;
 
-namespace notification_system.notification.Services.PushNoti
+namespace notification_system.notification.Services.PushNoti;
+
+public class PushNotiService : IPushNotiService
 {
-    public class PushNotiService : IPushNotiService
+    private readonly IUnitOfWork _unitOfWork;
+
+    public PushNotiService(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
+        _unitOfWork = unitOfWork;
+    }
 
-        public PushNotiService(IUnitOfWork unitOfWork)
+    public async Task<BaseResponse<PushNotiResponse>> PushNotiAsync(PushNotiRequest request, CancellationToken cs = default)
+    {
+        var message = new Message
         {
-            _unitOfWork = unitOfWork;
-        }
-
-        public async Task<BaseResponse<PushNotiResponse>> PushNotiAsync(PushNotiRequest request, CancellationToken cs = default)
-        {
-            var message = new Message
+            Token = request.DeviceToken,
+            Notification = new Notification
             {
-                Token = request.DeviceToken,
-                Notification = new Notification
+                Title = request.Title,
+                Body = request.Body,
+                ImageUrl = request.ImageUrl,
+            },
+            Data = new Dictionary<string, string>
+            {
+                { "deepLink", request.DeepLink ?? string.Empty },
+            },
+            Android = new AndroidConfig
+            {
+                Priority = Priority.High,
+                Notification = new AndroidNotification
                 {
                     Title = request.Title,
                     Body = request.Body,
-                    ImageUrl = request.ImageUrl,
+                    ImageUrl = request.ImageUrl
                 },
-                Data = new Dictionary<string, string>
-                {
-                    { "deepLink", request.DeepLink ?? string.Empty },
-                },
-                Android = new AndroidConfig
-                {
-                    Priority = Priority.High,
-                    Notification = new AndroidNotification
-                    {
-                        Title = request.Title,
-                        Body = request.Body,
-                        ImageUrl = request.ImageUrl
-                    },
-                },
-                Apns = new ApnsConfig
-                {
-                    Aps = new Aps
-                    {
-                        Alert = new ApsAlert { Title = request.Title, Body = request.Body },
-                        MutableContent = true,
-                    },
-                    FcmOptions = new ApnsFcmOptions { ImageUrl = request.ImageUrl },
-                },
-            };
-
-            var messaging = FirebaseMessaging.DefaultInstance;
-
-            var notiLog = new TblNotificationLog()
+            },
+            Apns = new ApnsConfig
             {
-                LogId = Ulid.NewUlid().ToString(),
-                LogType = NotificationTypeConstant.PushNoti,
-                Payload = request.ToJson(),
-                CreatedAt = DateTime.Now,
-            };
-            await _unitOfWork.NotificationLogRepository.AddAsync(notiLog, cs);
-            await _unitOfWork.SaveChangesAsync(cs);
+                Aps = new Aps
+                {
+                    Alert = new ApsAlert { Title = request.Title, Body = request.Body },
+                    MutableContent = true,
+                },
+                FcmOptions = new ApnsFcmOptions { ImageUrl = request.ImageUrl },
+            },
+        };
 
-            var response = await messaging.SendAsync(message, cs);
+        var messaging = FirebaseMessaging.DefaultInstance;
 
-            notiLog.ResponseAt = DateTime.Now;
-            notiLog.IsSuccess = true;
-            notiLog.ResponseMessage = response;
+        var notiLog = new TblNotificationLog()
+        {
+            LogId = Ulid.NewUlid().ToString(),
+            LogType = NotificationTypeConstant.PushNoti,
+            Payload = request.ToJson(),
+            CreatedAt = DateTime.Now,
+        };
+        await _unitOfWork.NotificationLogRepository.AddAsync(notiLog, cs);
+        await _unitOfWork.SaveChangesAsync(cs);
 
-            _unitOfWork.NotificationLogRepository.Update(notiLog);
-            await _unitOfWork.SaveChangesAsync(cs);
+        var response = await messaging.SendAsync(message, cs);
 
-            return BaseResponse<PushNotiResponse>.Success();
-        }
+        notiLog.ResponseAt = DateTime.Now;
+        notiLog.IsSuccess = true;
+        notiLog.ResponseMessage = response;
+
+        _unitOfWork.NotificationLogRepository.Update(notiLog);
+        await _unitOfWork.SaveChangesAsync(cs);
+
+        return BaseResponse<PushNotiResponse>.Success();
     }
 }
