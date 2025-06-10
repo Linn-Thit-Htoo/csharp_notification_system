b8523a80f1b7517d914802063ab87c862996e4ea
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-10 23:20:46 +0630

    -

--- notification_system.notification/Services/Kafka/MultipleEmailConsumerService.cs
+++ notification_system.notification/Services/Kafka/MultipleEmailConsumerService.cs
@@ -15,7 +15,11 @@ public class MultipleEmailConsumerService : BackgroundService
     private readonly AppSetting _appSetting;
     private readonly IServiceScopeFactory _serviceScopeFactory;
 
-    public MultipleEmailConsumerService(ILogger<MultipleEmailConsumerService> logger, IOptions<AppSetting> setting, IServiceScopeFactory serviceScopeFactory)
+    public MultipleEmailConsumerService(
+        ILogger<MultipleEmailConsumerService> logger,
+        IOptions<AppSetting> setting,
+        IServiceScopeFactory serviceScopeFactory
+    )
     {
         _logger = logger;
         _appSetting = setting.Value;
@@ -24,16 +28,19 @@ public class MultipleEmailConsumerService : BackgroundService
         {
             BootstrapServers = _appSetting.Kafka.BootstrapServers,
             GroupId = _appSetting.Kafka.Email.MultipleEmail.GroupId,
-            AutoOffsetReset = AutoOffsetReset.Earliest
+            AutoOffsetReset = AutoOffsetReset.Earliest,
         };
 
         _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
         _serviceScopeFactory = serviceScopeFactory;
     }
 
-    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await Extension.EnsureTopicExistsAsync(_appSetting.Kafka.BootstrapServers, _appSetting.Kafka.Email.MultipleEmail.Topic);
+        await Extension.EnsureTopicExistsAsync(
+            _appSetting.Kafka.BootstrapServers,
+            _appSetting.Kafka.Email.MultipleEmail.Topic
+        );
         _consumer.Subscribe(_appSetting.Kafka.Email.MultipleEmail.Topic);
 
         while (!stoppingToken.IsCancellationRequested)
