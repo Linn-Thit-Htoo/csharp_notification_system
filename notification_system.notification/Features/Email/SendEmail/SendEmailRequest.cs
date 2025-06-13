e1db1b346c19e6e76f0b41a07964d7b926bf0e9e
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-13 23:07:31 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendEmailRequest.cs
+++ notification_system.notification/Features/Email/SendEmail/SendEmailRequest.cs
@@ -1,6 +1,4 @@
-﻿using notification_system.notification.Models;
-
-namespace notification_system.notification.Features.Email.SendEmail;
+﻿namespace notification_system.notification.Features.Email.SendEmail;
 
 public class SendEmailRequest
 {
