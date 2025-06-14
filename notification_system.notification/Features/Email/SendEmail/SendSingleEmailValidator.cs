b4a48f03646d7c2401c4dadfc8b65575ce223077
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-14 09:41:04 +0630

    -

--- notification_system.notification/Features/Email/SendEmail/SendSingleEmailValidator.cs
+++ notification_system.notification/Features/Email/SendEmail/SendSingleEmailValidator.cs
@@ -1,6 +1,4 @@
-﻿using FluentValidation;
-
-namespace notification_system.notification.Features.Email.SendEmail;
+﻿namespace notification_system.notification.Features.Email.SendEmail;
 
 public class SendSingleEmailValidator : AbstractValidator<SendEmailRequest>
 {
