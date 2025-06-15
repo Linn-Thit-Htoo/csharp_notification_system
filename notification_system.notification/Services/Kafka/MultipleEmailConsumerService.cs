2bb32a796a94324229e7290085933a5370f61e9f
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:53:09 +0630

    -

--- notification_system.notification/Services/Kafka/MultipleEmailConsumerService.cs
+++ notification_system.notification/Services/Kafka/MultipleEmailConsumerService.cs
@@ -1,5 +1,4 @@
-﻿using notification_system.notification.Services.EmailServices;
-using static notification_system.notification.Extensions.Extension;
+﻿using static notification_system.notification.Extensions.Extension;
 
 namespace notification_system.notification.Services.Kafka;
 
