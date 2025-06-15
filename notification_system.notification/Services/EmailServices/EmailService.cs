e3111a376436bc104af97b0fc032a7d5a37e17e1
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:51:24 +0630

    -

--- notification_system.notification/Services/EmailServices/EmailService.cs
+++ notification_system.notification/Services/EmailServices/EmailService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using notification_system.notification.Configurations;
 using notification_system.notification.Constants;
 using notification_system.notification.Entities;
