bdbe13f697a736efc97d9cf03010707b6db9cb3a
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:53:57 +0630

    -

--- notification_system.notification/Services/Kafka/PushNotiConsumerService.cs
+++ notification_system.notification/Services/Kafka/PushNotiConsumerService.cs
@@ -1,5 +1,4 @@
-﻿using notification_system.notification.Services.PushNoti;
-
+﻿
 namespace notification_system.notification.Services.Kafka;
 
 public class PushNotiConsumerService : BackgroundService
