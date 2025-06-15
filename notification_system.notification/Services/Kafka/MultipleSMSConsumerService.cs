acf5465c9a8078956a679460108de73dee8c4ff0
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:53:36 +0630

    -

--- notification_system.notification/Services/Kafka/MultipleSMSConsumerService.cs
+++ notification_system.notification/Services/Kafka/MultipleSMSConsumerService.cs
@@ -1,5 +1,4 @@
-﻿using notification_system.notification.Features.SMS.SendSMS;
-using static notification_system.notification.Extensions.Extension;
+﻿using static notification_system.notification.Extensions.Extension;
 
 namespace notification_system.notification.Services.Kafka;
 
