feda1740e7cd753f56eaf57ebd05a3fb71c493dc
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:55:47 +0630

    -

--- notification_system.notification/Services/PushNoti/IPushNotiService.cs
+++ notification_system.notification/Services/PushNoti/IPushNotiService.cs
@@ -1,7 +1,4 @@
-﻿using notification_system.notification.Features.PushNoti;
-using notification_system.notification.Utils;
-
-namespace notification_system.notification.Services.PushNoti;
+﻿namespace notification_system.notification.Services.PushNoti;
 
 public interface IPushNotiService
 {
