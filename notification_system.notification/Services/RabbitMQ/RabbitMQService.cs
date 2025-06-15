c490339e9e9f3537d57cfd04fdaa1c2653e23932
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:56:19 +0630

    -

--- notification_system.notification/Services/RabbitMQ/RabbitMQService.cs
+++ notification_system.notification/Services/RabbitMQ/RabbitMQService.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using notification_system.notification.Configurations;
 using notification_system.notification.Extensions;
 using notification_system.notification.Features.Email.SendEmail;
 using notification_system.notification.Features.PushNoti;
