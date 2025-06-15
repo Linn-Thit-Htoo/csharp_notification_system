ee5c7a52015cacc11f4020c83e50d054011aac39
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:54:03 +0630

    -

--- notification_system.notification/Services/Kafka/SingleEmailConsumerService.cs
+++ notification_system.notification/Services/Kafka/SingleEmailConsumerService.cs
@@ -1,6 +1,5 @@
 ﻿using Confluent.Kafka;
 using Microsoft.Extensions.Options;
-using notification_system.notification.Configurations;
 using notification_system.notification.Extensions;
 using notification_system.notification.Features.Email.SendEmail;
 using notification_system.notification.Services.EmailServices;
