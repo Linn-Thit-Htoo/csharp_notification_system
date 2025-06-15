390cd26dd6713566c14ddd96b1b640285ab59cb3
Lin Thit Htoo <lth1212001@gmail.com>
2025-06-15 09:57:03 +0630

    -

--- notification_system.notification/Services/SMSServices/ITwilioService.cs
+++ notification_system.notification/Services/SMSServices/ITwilioService.cs
@@ -1,6 +1,4 @@
-﻿using notification_system.notification.Features.SMS.SendSMS;
-
-namespace notification_system.notification.Services.SMSServices;
+﻿namespace notification_system.notification.Services.SMSServices;
 
 public interface ITwilioService
 {
